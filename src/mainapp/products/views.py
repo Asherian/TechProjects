28f7d19beddc3fbb09a8f52fc5fdb23e6da24831
Asherian <Asherian@outlook.com>
2020-06-21 13:43:17 -0700

    Current Version
    Updates through video 13 of django tutorial

--- src/mainapp/products/views.py
+++ src/mainapp/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from . import views
 
-# Create your views here.
+
+def admin_console(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_page.html', {'products': products}),
+
